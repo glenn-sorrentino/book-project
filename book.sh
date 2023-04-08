905434af32577fa2d983a6815dfc787d1ab2a7ad
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-04-07 23:25:32 -0700

    Update book.sh

--- book.sh
+++ book.sh
@@ -1,77 +1,64 @@
 #!/bin/bash
 
-# Update the system
-sudo apt update && sudo apt upgrade -y
+# Update system
+echo "Updating system..."
+apt-get update && apt-get -y dist-upgrade && apt -y autoremove
 
 # Install dependencies
-sudo apt install -y python3 python3-pip python3-venv tor nginx
+echo "Installing dependencies..."
+apt install -y python3 python3-pip python3-venv nginx tor
 
-# Create a virtual environment for Python
+# Create a virtual environment
+echo "Creating a virtual environment..."
 python3 -m venv venv
-
-# Activate the virtual environment
 source venv/bin/activate
 
 # Install Python packages
-pip install requests beautifulsoup4
+echo "Installing Python packages..."
+pip install Flask beautifulsoup4 requests
 
-# Create the necessary directories
-mkdir -p root/css root/chapters
+# Create project directory structure
+echo "Creating project directory structure..."
+mkdir -p book_project/{css,chapters}
+touch onion-press/css/style.css
+touch onion-press/chapters/toc.html
+touch onion-press/cover.html
 
-# Create a basic CSS file
-tee > root/css/style.css << EOL
-body {
-  font-family: Arial, sans-serif;
-  font-size: 16px;
-  line-height: 1.5;
-}
-EOL
-
-# Tor onion service configuration
-tee >> /etc/tor/torrc << EOL
-RunAsDaemon 1
-HiddenServiceDir /var/lib/tor/myonionservice/
-HiddenServicePort 80 127.0.0.1:8080
-EOL
-
-sudo systemctl restart tor
-sleep 10
-
-sudo ln -sf /etc/nginx/sites-available/myonionservice.nginx /etc/nginx/sites-enabled/
-sudo nginx -t && sudo systemctl restart nginx
-
-# Set up Nginx and Tor onion service
-
-# Nginx configuration
-tee > /etc/nginx/sites-available/myonionservice.nginx << EOL
+# Configure Nginx
+echo "Configuring Nginx..."
+sudo rm /etc/nginx/sites-enabled/default
+sudo bash -c 'cat > /etc/nginx/sites-available/onion-press << EOL
 server {
     listen 80;
     server_name localhost;
-    location / {
-        proxy_pass http://127.0.0.1:8080;
-        proxy_set_header Host \$host;
-        proxy_set_header X-Real-IP \$remote_addr;
-        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto \$scheme;
-        proxy_connect_timeout 300s;
-        proxy_send_timeout 300s;
-        proxy_read_timeout 300s;
-    }
-    
-        add_header Strict-Transport-Security "max-age=63072000; includeSubdomains";
-        add_header X-Frame-Options DENY;
-        add_header Onion-Location http://$ONION_ADDRESS\$request_uri;
-        add_header X-Content-Type-Options nosniff;
-        add_header Content-Security-Policy "default-src 'self'; frame-ancestors 'none'";
-        add_header Permissions-Policy "geolocation=(), midi=(), notifications=(), push=(), sync-xhr=(), microphone=(), camera=(), magnetometer=(), gyroscope=(), speaker=(), vibrate=(), fullscreen=(), payment=(), interest-cohort=()";
-        add_header Referrer-Policy "no-referrer";
-        add_header X-XSS-Protection "1; mode=block";
-}
-EOL
+    root /book_project;
+    index cover.html;
 
-sudo ln -s /etc/nginx/sites-available/myonionservice /etc/nginx/sites-enabled/
-sudo nginx -t
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    location /chapters {
+        autoindex on;
+    }
+}
+EOL'
+sudo ln -s /etc/nginx/sites-available/onion-press /etc/nginx/sites-enabled/
 sudo systemctl restart nginx
 
-# Display the onion service address
-sudo cat /var/lib/tor/myonionservice/hostname
+# Configure Tor Onion Service
+echo "Configuring Tor Onion Service..."
+sudo bash -c 'cat >> /etc/tor/torrc << EOL
+HiddenServiceDir /var/lib/tor/onion-press/
+HiddenServiceVersion 3
+HiddenServicePort 80 127.0.0.1:80
+EOL'
+sudo systemctl restart tor
+
+# Display Onion address
+echo "Displaying Onion address..."
+sudo cat /var/lib/tor/onion-press/hostname
+
+# Instructions
+echo "Installation complete! To start working on your project, activate the virtual environment by running 'source venv/bin/activate'."
+python app.py
